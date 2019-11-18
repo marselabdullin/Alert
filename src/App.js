195df80f7a32185364ad0322d023603717e4367e
marselabdullin <abdmars@mail.ru>
2019-11-18 18:26:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    alertColor: ''
+  }
+
+  randomAlert() {
+    const randomNum = (Math.ceil(Math.random() * 100));
+    if( randomNum <= 33) {
+      this.setState({
+        alertColor: 'success'
+      })
+    } else if (randomNum >= 34 && randomNum <= 66) {
+      this.setState({
+        alertColor: 'danger'
+      })
+    } else {
+      this.setState({
+        alertColor: 'warning'
+      })
+    }
+  }
+
+  render() {
+    let {alertColor} = this.state;
+
+    const classes=`alert alert-${alertColor}`;
+    const alertMessage = (
+      <div className={classes} role="alert">
+        Уведомление
+      </div>
+    )
+
+    return (
+      <div className="container">
+        <header className="App-header">
+          {alertColor ? alertMessage : null}
+        </header>
+        <div className="d-flex justify-content-center">
+          <button 
+            type="button" 
+            className="btn btn-secondary d-flex"
+            onClick={() => this.randomAlert()}
+          >
+            Получить уведомление!
+          </button> 
+        </div>
+      </div>
+    );
+  } 
 }
-
-export default App;
